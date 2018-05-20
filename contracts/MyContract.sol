4729e19e67ee4d1bfc587edfa1ad79f6d5bfed11
Makoto <inouemak@googlemail.com>
2018-05-20 19:13:32 +0100

    Add MyContract

---
+++ contracts/MyContract.sol
@@ -0,0 +1,9 @@
+import "zos-lib/contracts/migrations/Migratable.sol";
+
+contract MyContract is Migratable {
+  uint256 public x;
+
+  function initialize(uint256 _x) isInitializer("MyContract", "0") public {
+    x = _x;
+  }
+}
